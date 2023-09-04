9e4ebe805265117efd1ea96a84b3e08beaf8037c
99073048 <99073048@mydavinci.nl>
2023-09-04 09:40:10 +0200

    changes

---
+++ Deel_1/Les_3/uitwerkingen/les_3_opd_4.py/Madlibs.py
@@ -0,0 +1,17 @@
+naam_beste_vriend = "Piet"
+kleuterprogramma = "Sesamstraat"
+
+print(naam_beste_vriend + " geeft een super pyjamafeestje met als thema " + kleuterprogramma + " dat word dus de hele avond " + kleuterprogramma +" kijken.")
+
+woord_1 = "mooie"
+stad_1 = "Rotterdam"
+naam_1 = "Jayden"
+Woord_3 = "kledingwinkel"
+woord_4 = "verkoper"
+woord_5 = "jas"
+woord_6 = "parkje"
+woord_zin_7 = "Voetbal zaten te spelen"
+
+print ("Het was een" woord_1 +  " dag in " stad_1. + naam_1 + " liep door de straten, op zoek naar een " + Woord_3 . +  " Plotseling " + naam_1 + "je een" woord_4 man/vrouw die een (woord_5) verkocht. (naam_1) besloot om het te kopen en liep verder. Toen (naam_1) bij een (woord_6) kwam, zag hij/zij een groep mensen die (woord_zin_7). (naam_1) besloot om mee te doen en had de tijd van zijn/haar leven.)
+
+
