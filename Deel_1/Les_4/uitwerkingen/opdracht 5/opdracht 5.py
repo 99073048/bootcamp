e30eacf28990e6539a8a9b6842d321aa14c68c75
99073048 <99073048@mydavinci.nl>
2023-09-04 15:51:14 +0200

    create opdracht 5

---
+++ Deel_1/Les_4/uitwerkingen/opdracht 5/opdracht 5.py
@@ -0,0 +1,10 @@
+Opdracht 5:
+Schrijf een programma met twee variabelen.
+Geef de eerste variabele de waarde 25 en de tweede 0.
+Deel variabele een door variabele twee. 
+Wat gebeurt er? 
+
+a= float { input"25 "}
+
+
+ a = " 25 " 
