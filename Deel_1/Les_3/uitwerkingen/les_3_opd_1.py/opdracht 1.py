57c2aa09268ebfc0d8d8dfe63157ea7a8b28783b
99073048 <99073048@mydavinci.nl>
2023-09-04 14:55:07 +0200

    fruit

--- Deel_1/Les_3/uitwerkingen/les_3_opd_1.py/opdracht 1.py
+++ Deel_1/Les_3/uitwerkingen/les_3_opd_1.py/opdracht 1.py
@@ -1,18 +1,17 @@
-#Prijzen van het fruit
+# Prijzen van het fruit
 prijs_appels = 3.40
 prijs_druiven = 2.45
 prijs_bananen = 1.95
 
-#Bereken de totale prijs van het fruit
-totale_prijs = prijs_appels + prijs_druiven + prijs_bananen 
+# Bereken de totale prijs van het fruit
+totale_prijs = prijs_appels + prijs_druiven + prijs_bananen
 
-
-#BTW-tarief
-btw_tarief = 0.09
+# BTW-tarief
+btw_tarief = 9
 
 # Bereken de BTW
-btw_bedrag = totale_prijs * btw_tarief
+btw_bedrag = totale_prijs * (btw_tarief / 100)
 
-
-
-
+# Print de resultaten
+print("Totale prijs van het fruit: €", totale_prijs)
+print("BTW-bedrag: €", btw_bedrag)
