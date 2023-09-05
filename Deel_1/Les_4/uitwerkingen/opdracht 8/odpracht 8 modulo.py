5f4844ca6dcd22e49c03d17cd1c48e7c08f27934
99073048 <99073048@mydavinci.nl>
2023-09-05 23:59:01 +0200

    afronding opdracht 8

---
+++ Deel_1/Les_4/uitwerkingen/opdracht 8/odpracht 8 modulo.py
@@ -0,0 +1,27 @@
+# Schrijf een programma wat uitrekent hoeveel keer 13 (helemaal) in 625 past!
+# Bereken ook wat er overblijft.
+# (hint: gebruik de modulo en integer deling (of floor division))
+# Getal waarin we willen delen
+totaal = 625
+
+# Getal dat we willen delen
+deeltal = 13
+
+# Bereken hoe vaak deeltal in totaal past
+aantal_keer = totaal // deeltal 
+
+
+# berekening wat er overblijft met modulo 
+
+rest = totaal % deeltal
+
+# met behulp van AI
+# Print het resultaat
+print (f"{deeltal} past {aantal_keer} keer in {totaal}.")
+print (f"Er blijft {rest} over.")
+
+
+
+
+
+
