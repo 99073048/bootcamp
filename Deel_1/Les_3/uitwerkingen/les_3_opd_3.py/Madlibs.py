2de2530b9609245696ab8b1669656a1f5ebeafcd
99073048 <99073048@mydavinci.nl>
2023-09-05 19:16:21 +0200

    changes

--- Deel_1/Les_3/uitwerkingen/les_3_opd_3.py/Madlibs.py
+++ Deel_1/Les_3/uitwerkingen/les_3_opd_3.py/Madlibs.py
@@ -1,15 +1,14 @@
 
 
-
-woord_1 = "mooie"
-stad_1 = "Rotterdam"
-naam_1 = "Jayden"
-Woord_3 = "kledingwinkel"
+woord_1 = input(" mooie : ")
+stad_1 = input ( " noem de plaats "    )
+naam_1 = 
+woord_3 = "kledingwinkel"
 woord_4 = "verkoper"
 woord_5 = "jas"
 woord_6 = "parkje"
 woord_zin_7 = "Voetbal zaten te spelen"
 
-print ( "Het was een " + woord_1 + " dag in " + stad_1 + ". " + naam_1 + " liep door de straten, op zoek naar een " + Woord_3 + ". Plotseling zag " + naam_1 + " een " + woord_4 + " die een " + woord_5 + " verkocht. " + naam_1 + " besloot om het te kopen en liep verder. Toen " + naam_1 + " bij een " + woord_6 + " kwam, zag hij/zij een groep mensen die " + woord_zin_7 + ". " + naam_1 + " besloot om mee te doen en had de tijd van zijn/haar leven.")
+print ( "Het was een " + woord_1 + " dag in " + stad_1 + ". " + naam_1 + " liep door de straten, op zoek naar een " + woord_3 + ". Plotseling zag " + naam_1 + " een " + woord_4 + " die een " + woord_5 + " verkocht. " + naam_1 + " besloot om het te kopen en liep verder. Toen " + naam_1 + " bij een " + woord_6 + " kwam, zag hij/zij een groep mensen die " + woord_zin_7 + ". " + naam_1 + " besloot om mee te doen en had de tijd van zijn/haar leven.")
 
 
--- Deel_1/Les_4/uitwerkingen/opdracht 5/opdracht 5.py
+++ Deel_1/Les_4/uitwerkingen/opdracht 5/opdracht 5.py
@@ -1,12 +1,11 @@
-Opdracht 5:
-Schrijf een programma met twee variabelen.
-Geef de eerste variabele de waarde 25 en de tweede 0.
-Deel variabele een door variabele twee. 
-Wat gebeurt er? 
+# Opdracht 5:
+# Schrijf een programma met twee variabelen.
+# Geef de eerste variabele de waarde 25 en de tweede 0.
+# Deel variabele een door variabele twee. 
+# Wat gebeurt er? 
 
-a= float { input"25 "}
+a= 25
 
+b= 0
 
- a = " 25 " 
-
- b= 
+print( a/b )
