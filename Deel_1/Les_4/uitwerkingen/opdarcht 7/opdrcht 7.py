40082340ce04f64b0f8022e3edce46733c5eeb32
99073048 <99073048@mydavinci.nl>
2023-09-05 22:58:45 +0200

    afronding opdracht 7

---
+++ Deel_1/Les_4/uitwerkingen/opdarcht 7/opdrcht 7.py
@@ -0,0 +1,10 @@
+
+print( (431 / 100) * 100 )
+
+# Wat denk je dat eruit komt? En test het daarna eens!
+
+# Als eerst woord het variabel 431 gedeeld door 100 wat 4,31 word en vervolgens word vermenigvuldigt door 100 wat op 430,99999 komt 
+
+
+# Wat gebeurt hier? Schrijf dit op als commentaar!
+
