5eb4fcd05bb5d20bc3c28e2ae573fff32ebf5af1
99073048 <99073048@mydavinci.nl>
2023-09-06 00:21:34 +0200

    seconden per jaar opdracht 10

---
+++ Deel_1/Les_4/uitwerkingen/uitrekening per jaar.py
@@ -0,0 +1,13 @@
+seconden_per_minuut= 60
+Seconden_per_uur= 60*seconden_per_minuut
+seconden_per_dag= 24* Seconden_per_uur
+
+#aantal dagen in een jaar
+Dagen_per_jaar= 365
+
+# aantal seconden in een jaar
+
+seconden_per_jaar= Dagen_per_jaar*seconden_per_dag
+
+
+print ( f" er zitten {seconden_per_jaar} seconden in een jaar." )
