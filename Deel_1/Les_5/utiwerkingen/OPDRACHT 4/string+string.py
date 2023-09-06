a05b006ae4bf9451bd8e2e41afd5b18ede1be167
99073048 <99073048@mydavinci.nl>
2023-09-06 13:33:36 +0200

    afronding les 5

---
+++ Deel_1/Les_5/utiwerkingen/OPDRACHT 4/string+string.py
@@ -0,0 +1,31 @@
+
+# Opdracht 4:
+# Schrijf code die  berekent, gebruik makend
+# van variabelen straal en pi = Voor het geval je het vergeten bent, de formule is
+
+# Toon de uitkomst als volgt: “De oppervlakte van een cirkel met straal ... is ...”
+
+# Pas je programma vervolgens zo aan dat de gebruiker een straal kan invoeren en je programma vervolgens de oppervlakte uitrekent.
+# Extra: zorg ervoor dat je programma behalve de oppervlakte ook de omtrek toont. (hoe bereken je deze?)
+
+# de oppervlakte van een cirkel
+
+
+
+# int gebruik je in dit geval omdat twee strings eigelijk niet samen kunnen werken zonder() INT )
+
+
+pi= 3.14159
+
+straal= float(input(" straal invoeren: "))
+
+# omtrek van een cirkel
+omtrek= 2*pi*straal
+
+# de oppervlakte van een cirkel
+oppervlakte= pi* straal**2
+
+ 
+print (F"De oppervlakte van een cirkel met straal{straal} is {omtrek}")
+print (F"De omtrek van een cirkel met behulp van straal {straal} is {omtrek}" )
+
---
+++ Deel_1/Les_6/opdracht 1 cijfers met strings.py
@@ -0,0 +1,23 @@
+
+# boole,boolean
+
+
+breedte= int(input( "hoe breed is uw wagen? "))
+
+print ("toegangscontrole")
+
+print(breedte <= 1200)
+
+
+if breedte <= 2900:
+print(" beste chauffeur u mag door ")
+
+
+
+
+
+
+# groter dan =  ( <= )    1000
+# kleiner Dan = ( >= )
+# stoppen bij = ( < )     
+
