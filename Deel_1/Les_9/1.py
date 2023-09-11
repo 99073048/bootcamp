0e68875bd29bfa696e17067949cfc3793acbadd5
99073048 <99073048@mydavinci.nl>
2023-09-11 11:57:26 +0200

    true not true

---
+++ Deel_1/Les_9/1.py
@@ -0,0 +1,23 @@
+Opdracht 1:
+Leg het verschil uit:
+a=5
+b=3
+c=2
+if (a==6 and b==4 or c==2):
+   print("De conditie is waar")
+else: 
+   print("De conditie is niet waar")	
+
+En: 
+a=5
+b=3
+c=2
+if (a==6 and (b==4 or c==2)):
+   print("De conditie is waar")
+else:
+   print("De conditie is niet waar")
+
+# (geef met commentaar aan in de code wat het verschil is)
+
+# hier legt het uit dat als een van de variabelen gelijk is aan de variabele nummer dan is de code true 
+# als geen een van de condities gelijk is is de code niet true
