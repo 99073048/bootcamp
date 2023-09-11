89895a51a749b3e26fc6c3349816e9a1cf8beca8
99073048 <99073048@mydavinci.nl>
2023-09-11 15:43:33 +0200

    create opdracht 2

---
+++ tussentoets/opdracht2.py/opdracht2.py/Opdracht 2.py
@@ -0,0 +1,6 @@
+print(6 > 3 and 2 < 3)           # print: True
+print(3 > 6 or 2 < 3)            # print: True
+print('a' == 'a' or 'a' == 'b')  # print: True
+print(9 > 3 and 5 > 4)           # print: True
+
+# opdracht 2 and or in
