2c5aea89587453489501edf729da6a3ec2b544c1
99073048 <99073048@mydavinci.nl>
2023-09-11 15:42:54 +0200

    opdracht 1

---
+++ tussentoets/opdracht1.py/opdracht1.py
@@ -0,0 +1,9 @@
+# 1de expressies af (vul de juiste operator in)
+# Let op: soms zijn er meerdere mogelijkheden, 1 antwoord is genoeg.
+# opdracht 1
+print(6 * 3) # toont: 18
+print(15-13) 	# toont: 2
+print(3 / 3) 	# toont: 1
+print(16// 3)   # toont: 1
+print(6 % 3) 	# toont: 216
+
---
+++ tussentoets/opdracht3.py/opdracht3.py
@@ -0,0 +1,13 @@
+
+# vraag de gebruikers naam
+naam=input("voer uw naam hier in:")
+
+
+# vraag de gebruiker om zijn leeftijd
+leeftijd= int (input("voer uw leeftijd in"))
+
+if leeftijd < 18:
+   print (f" Beste {naam}, je bent nog geen 18. Alleen autorijden zit er dus niet in")
+
+else:
+   print (f" Beste {naam}, je bent 18 of ouder en mag dus alleen autorijden met rijbewijs althans")
